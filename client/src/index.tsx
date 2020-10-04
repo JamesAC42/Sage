16688058997ba4da4221cf23d271900bd9f27611
William Allen <dalearn@icloud.com>
2020-10-04 19:56:53 -0400

    Move most colors to settings file
    This commit moves most of the colors to the new settings.scss file and switches much of the existing code around to make use of the useful nesting features included with SCSS.  This work will continue in the next commit.

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
+import './css/index.scss';
 import App from './js/App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
