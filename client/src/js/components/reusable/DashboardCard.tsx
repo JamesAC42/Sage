aba29bed70dd22eff905da4433868ae84cde797f
William Allen <dalearn@icloud.com>
2020-11-03 23:34:29 -0500

    Add author name, hover effect, and date modified to dashboard cards

--- client/src/js/components/reusable/DashboardCard.tsx
+++ client/src/js/components/reusable/DashboardCard.tsx
@@ -4,6 +4,7 @@ import '../../../css/dashboard-card.scss';
 
 interface DashboardCardProps {
   name: string,
+  author: string,
   previewImageURL: string
 }
 
@@ -12,8 +13,12 @@ export default class DashboardCard extends Component<DashboardCardProps> {
         return(
             <div className="card dashboard-card">
               <img className="card-image-top preview" src={this.props.previewImageURL}></img>
+              <div className="date-overlay">Last Modified Today</div>
               <div className="card-header">
                 {this.props.name}
+              </div>
+              <div className="text-subtle">
+                {this.props.author}
               </div>
             </div>
         )
