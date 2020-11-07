69d666c5a31297833909db650d12739e30c54117
James Crovo <jamescrovo2019@lschs.org>
2020-11-07 01:56:37 -0500

    Update homepage layout

--- client/src/js/components/Home.tsx
+++ client/src/js/components/Home.tsx
@@ -1,62 +1,149 @@
 import React, { Component } from 'react';
 import '../../css/home.scss';
-import '../../css/master.scss'; 
-import img1 from '../../images/d-1.jpg' 
-import img2 from '../../images/d-2.jpg'
-import img3 from '../../images/d-3.jpg'
-import img4 from '../../images/d-4.jpg' 
+
+import {
+	Slide,
+	slides1,
+	slides2,
+	slides3
+} from './data/slides';
+
+class SlideshowState {
+	active:number;
+	constructor() {
+		this.active = 0;
+	}
+}
+
+interface SlideshowProps {
+	slides:Array<Slide>,
+	reversed:boolean
+}
+
+class Slideshow extends Component<SlideshowProps> {
+	state:SlideshowState;
+	constructor(props:SlideshowProps) {
+		super(props);
+		this.state = new SlideshowState();
+	}
+	componentDidMount() {
+		setInterval(() => {
+			const next = this.state.active === this.props.slides.length - 1 ?
+				0 : this.state.active + 1;
+			this.setState({
+				active: next
+			});
+		}, 10000);
+	}
+	render(){
+		return(
+			<div className="presentation-row slideshow">
+				{
+					this.props.reversed ? 
+					<div className="col">
+						<img src={this.props.slides[this.state.active].img} alt={"Image"} />
+					</div> : null
+				}
+				<div className="col slide">
+					<div className="info-header">
+						{this.props.slides[this.state.active].header}
+					</div>
+					<ul className="info">
+						<li>{this.props.slides[this.state.active].text}</li>
+					</ul>
+				</div>
+				{
+					!this.props.reversed ? 
+					<div className="col">
+						<img src={this.props.slides[this.state.active].img} alt={"Image"} />
+					</div> : null
+				}
+			</div>
+		)
+	}
+}
+
+class HomeState {
+	scrollPercent:number;
+	constructor() {
+		this.scrollPercent = 0;
+	}
+}
 
 export default class Home extends Component {
+
+	state:HomeState;
+	constructor(props:{}) {
+		super(props);
+		this.state = new HomeState();
+	}
+
+	handleScroll = (e: React.UIEvent<HTMLElement>): void => {
+		const scrollPercent = 
+			e.currentTarget.scrollTop / (e.currentTarget.scrollHeight - e.currentTarget.clientHeight);
+		this.setState({scrollPercent});
+	}
+
     render() {
-		
+
+		const parallax = this.state.scrollPercent * 100;
+		const translate = "translate(-50%, calc(-50% + " + parallax + "vh)) rotate(45deg)";
         return(			
-			<div className="home-container">
-				<div className="header-card">
-					<div className="head">SAGE</div>
-					<div className="row">
-						{/* Column 1 */ }
+			<div
+				className="container container-fill scroll"
+				onScroll={this.handleScroll}>
+
+				<div className="home-content">
+					<div className="presentation-row title">
+						<div className="title-card">
+							<div className="title-card-inner">
+								Sage
+							</div>
+							<div 
+								className="title-card-background"
+								style={{
+									transform:translate
+								}}>
+							</div>
+						</div>
+					</div>
+					<div className="presentation-row info">
 						<div className="col">
-							<ul className="list-unstyled left">
-								<ul className="title1">WHAT IS SAGE:</ul>
-								<ul>Sage is a Web Based Data Visualization and Analytics Software for easily creating dashboards, monitoring trends, and generally making information available, digestible, and customizable.</ul>
+							<div className="info-header">What is Sage?</div>
+							<ul className="info">
+								<li>Sage is a Web Based Data Visualization and Analytics Software for easily creating dashboards, monitoring trends, and generally making information available, digestible, and customizable.</li>
 							</ul>
 						</div>
-						{/* Column 2 */}
 						<div className="col">
-							<ul className="list-unstyled left">
-								<ul className="title1">WHY SAGE:</ul>
-								<ul>Sage provides a user friendly, community driven service for consolidating, monitoring, and analyzing information from the level of large corporations to small organizations to the average consumer.</ul>
-								<ul>Sage accepts various types of endpoints including REST, RSS, ODATA, and spreadsheets, retrieve and consolidate the data for the user who can then create custom dashboards that can be published publicly or privately.</ul>
+							<div className="info-header">Why Sage?</div>
+							<ul className="info">
+								<li>Sage provides a user friendly, community driven service for consolidating, monitoring, and analyzing information from the level of large corporations to small organizations to the average consumer.</li>
+								<li>Sage accepts various types of endpoints including REST, RSS, ODATA, and spreadsheets, retrieve and consolidate the data for the user who can then create custom dashboards that can be published publicly or privately.</li>
 							</ul>
 						</div>
-						{/* Column 3 */}
 						<div className="col">
-							<ul className="list-unstyled left">
-								<ul className="title1">WHAT SAGE OFFERS:</ul>
-								<ul>Users can create accounts and publish custom reports using data from REST endpoints, RSS feeds, or even upload their own spreadsheets.</ul>
-								<ul>Check out the slideshow below for more details.</ul>
+							<div className="info-header">What Sage Offers</div>
+							<ul className="info">
+								<li>Users can create accounts and publish custom reports using data from REST endpoints, RSS feeds, or even upload their own spreadsheets.</li>
+								<li>Check out the slideshow below for more details.</li>
 							</ul>
 						</div>
 					</div>
+					
 				</div>
-				<div className="fling-minislide">
-					<div className="row">
-						{/* Column 1 */}
-						<div className="col2">
-							<img src={img4} alt="Slide 4" />
-							<img src={img3} alt="Slide 3" />
-							<img src={img2} alt="Slide 2" />
-							<img src={img1} alt="Slide 1" />
-						</div>
-						{/* Column 2 */}
-						<div className="col2">
-							<ul>HELLO</ul>
-						</div>
-					</div>
-				</div>
+
+				<Slideshow 
+					reversed={false}
+					slides={slides1}/>
+				<Slideshow 
+					reversed={true}
+					slides={slides2}/>
+				<Slideshow 
+					reversed={false}
+					slides={slides3}/>
+
 				<div className="footer-card"> 
 					<div className="row">
-						{/* Column1 */}
 						<div className="col">
 							<h4>SAGE</h4>
 							<ul>800-000-000</ul>
@@ -71,6 +158,6 @@ export default class Home extends Component {
 					</div>
 				</div>
 			</div>
-        );
+        )
     }
 }
---
+++ client/src/js/components/data/slides.ts
@@ -0,0 +1,80 @@
+import img1 from '../../../images/d-1.jpg';
+import img2 from '../../../images/d-2.jpg';
+import img3 from '../../../images/d-3.jpg';
+import img4 from '../../../images/d-4.jpg';
+
+export interface Slide {
+	header:string,
+	text:string,
+	img:string
+}
+
+export const slides1:Array<Slide> = [
+	{
+		header: 'Header 1',
+		text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit, nesciunt ipsum iure cupiditate quasi explicabo delectus mollitia numquam consequatur recusandae commodi. Provident voluptates explicabo consequuntur eligendi, reprehenderit numquam natus, ducimus nisi eveniet fugit reiciendis aperiam cupiditate, harum quaerat minima incidunt debitis dolorem magnam odit sequi officiis cum. Ea, ut quo?",
+		img: img1
+	},
+	{
+		header: 'Header 2',
+		text: "Lorem ipsum, dolor sit ame ut quo? tetur adipisicing elit. Fugit, ",
+		img: img2
+	},
+	{
+		header: 'Header 3',
+		text: "Lorem ipsum, dolor sit amet cons, ut quo? Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores unde assumenda officia, eveniet, porro at et cumque nobis iure soluta quae rem excepturi dolorem esse dolorum? Eveniet corporis laboriosam tempore sapiente id sequi, natus commodi deleniti aliquam dolorem deserunt, unde illo saepe nulla facilis voluptate! Excepturi suscipit fuga eius nesciunt!",
+		img: img3
+	},
+	{
+		header: 'Header 4',
+		text: "Lorem ipsum, dolor sit amet sum, dolor sit ame ut quo? tetur adipisicing elit. Fugit sum, dolor sit ame ut quo? tetur adipisicing elit. Fugit consectetur adipisicing elit. Fugit, nesciunt ipsum iure cupiditate quasi explicabo delectus mollitia numquam consequatur recusandae commodi. Provident voluptates explicabo consequuntur eligendi, reprehenderit numquam natus, ducimus nisi eveniet fugit reiciendis aperiam cupiditate, harum quaerat minima incidunt debitis dolorem magnam odit sequi officiis cum. Ea, ut quo?",
+		img: img4
+	}
+];
+
+export const slides2:Array<Slide> = [
+	{
+		header: 'Header 1',
+		text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit, nesciunt ipsum iure cupiditate quasi explicabo delectus mollitia numquam consequatur recusandae commodi. Provident voluptates explicabo consequuntur eligendi, reprehenderit numquam natus, ducimus nisi eveniet fugit reiciendis aperiam cupiditate, harum quaerat minima incidunt debitis dolorem magnam odit sequi officiis cum. Ea, ut quo?",
+		img: img2
+	},
+	{
+		header: 'Header 2',
+		text: "Lorem ipsum, dolor sit ame ut quo? tetur adipisicing elit. Fugit, ",
+		img: img4
+	},
+	{
+		header: 'Header 3',
+		text: "Lorem ipsum, dolor sit amet cons, ut quo? Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores unde assumenda officia, eveniet, porro at et cumque nobis iure soluta quae rem excepturi dolorem esse dolorum? Eveniet corporis laboriosam tempore sapiente id sequi, natus commodi deleniti aliquam dolorem deserunt, unde illo saepe nulla facilis voluptate! Excepturi suscipit fuga eius nesciunt!",
+		img: img1
+	},
+	{
+		header: 'Header 4',
+		text: "Lorem ipsum, dolor sit amet sum, dolor sit ame ut quo? tetur adipisicing elit. Fugit sum, dolor sit ame ut quo? tetur adipisicing elit. Fugit consectetur adipisicing elit. Fugit, nesciunt ipsum iure cupiditate quasi explicabo delectus mollitia numquam consequatur recusandae commodi. Provident voluptates explicabo consequuntur eligendi, reprehenderit numquam natus, ducimus nisi eveniet fugit reiciendis aperiam cupiditate, harum quaerat minima incidunt debitis dolorem magnam odit sequi officiis cum. Ea, ut quo?",
+		img: img3
+	}
+];
+
+export const slides3:Array<Slide> = [
+	{
+		header: 'Header 1',
+		text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit, nesciunt ipsum iure cupiditate quasi explicabo delectus mollitia numquam consequatur recusandae commodi. Provident voluptates explicabo consequuntur eligendi, reprehenderit numquam natus, ducimus nisi eveniet fugit reiciendis aperiam cupiditate, harum quaerat minima incidunt debitis dolorem magnam odit sequi officiis cum. Ea, ut quo?",
+		img: img3
+	},
+	{
+		header: 'Header 2',
+		text: "Lorem ipsum, dolor sit ame ut quo? tetur adipisicing elit. Fugit, ",
+		img: img1
+	},
+	{
+		header: 'Header 3',
+		text: "Lorem ipsum, dolor sit amet cons, ut quo? Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores unde assumenda officia, eveniet, porro at et cumque nobis iure soluta quae rem excepturi dolorem esse dolorum? Eveniet corporis laboriosam tempore sapiente id sequi, natus commodi deleniti aliquam dolorem deserunt, unde illo saepe nulla facilis voluptate! Excepturi suscipit fuga eius nesciunt!",
+		img: img4
+	},
+	{
+		header: 'Header 4',
+		text: "Lorem ipsum, dolor sit amet sum, dolor sit ame ut quo? tetur adipisicing elit. Fugit sum, dolor sit ame ut quo? tetur adipisicing elit. Fugit consectetur adipisicing elit. Fugit, nesciunt ipsum iure cupiditate quasi explicabo delectus mollitia numquam consequatur recusandae commodi. Provident voluptates explicabo consequuntur eligendi, reprehenderit numquam natus, ducimus nisi eveniet fugit reiciendis aperiam cupiditate, harum quaerat minima incidunt debitis dolorem magnam odit sequi officiis cum. Ea, ut quo?",
+		img: img2
+	}
+];
+
